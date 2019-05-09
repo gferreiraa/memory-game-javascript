ee6ab294b06e2090db6cc9f0160a8fa6340c9a96
gferreira <gferreiraa@gmail.com>
2019-05-09 00:11:39 -0300

    Part 2: Ok - Game complete features

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle('flip')
+  this.classList.add('flip')
   console.log('I was cliked');
   console.log(this); // this representa o elemento
+  if( !hasFlippedCard ){
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+    // do cards match?
+    if(firstCard.dataset.framework === secondCard.dataset.framework) {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+    }else{
+    // not a match
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+    }, 1500);
+    }
+    console.log('Function was executed')
+  }
 }
 
 cards.forEach( card => card.addEventListener('click', flipCard))
